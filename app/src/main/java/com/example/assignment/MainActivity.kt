20e1d812949c88924168422bf24993e402727e3e
cmk <cmk170307770@gmail.com>
2022-06-03 01:57:45 +0800

    Add Http API Request

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import okhttp3.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private val client = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var data = callHKOApi("rhrread");
+        println( "cmkTest: " + data)
+    }
+
+    fun callHKOApi(dataType: String){
+        var rainfall = 0;
+        val request = Request.Builder()
+            .url(getString(R.string.hko_api) + dataType)
+            .build();
+
+        return client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) {
+                var rainfallList = JSONArray(JSONObject(response.body()?.string())["rainfall"]);
+                
+            }
+
+        })
     }
 }
