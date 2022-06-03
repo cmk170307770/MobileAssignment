33458e72c9539159caead0350e46b31b2086c0c7
cmk <cmk170307770@gmail.com>
2022-06-04 02:52:27 +0800

    Add Firebase Auth function

--- app/src/main/java/com/example/assignment/LoginActivity.kt
+++ app/src/main/java/com/example/assignment/LoginActivity.kt
@@ -1,11 +1,43 @@
 package com.example.assignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth;
+    var emailPlainText : EditText? = null;
+    var passwordPlainText : EditText? = null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    fun onLoginButtonClick(view: View){
+        emailPlainText = this.findViewById(R.id.emailPlainText);
+        passwordPlainText = this.findViewById(R.id.passwordPlainText);
+        val email = this.emailPlainText?.text.toString();
+        val password = this.passwordPlainText?.text.toString();
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+            task ->
+                if(task.isSuccessful){
+                    val intent = Intent(this, MainActivity:: class.java);
+                    startActivity(intent);
+                    finish()
+                }
+        }.addOnFailureListener{ exception ->
+            Toast.makeText(applicationContext, exception.localizedMessage,Toast.LENGTH_LONG).show();
+
+        }
+
     }
 }
--- app/src/main/java/com/example/assignment/RegisterActivity.kt
+++ app/src/main/java/com/example/assignment/RegisterActivity.kt
@@ -1,11 +1,47 @@
 package com.example.assignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth;
+    var registerEmailPlainText : EditText? = null;
+    var registerPasswordPlainText : EditText? = null;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register2)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    fun onRegisterButtonClick(view: View){
+        registerEmailPlainText = this.findViewById(R.id.registerEmailPlainText);
+        registerPasswordPlainText = this.findViewById(R.id.registerPasswordPlainText);
+
+        var email = this.registerEmailPlainText?.text.toString();
+        var password = this.registerPasswordPlainText?.text.toString();
+
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                var intent = Intent(this,LoginActivity::class.java);
+                startActivity(intent);
+                finish();
+            }
+        }.addOnFailureListener{ exception ->
+            Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()
+        };
+    }
+
+    fun onLoginButtonClick(view: View){
+        var intent = Intent(this, LoginActivity::class.java);
+        startActivity(intent);
     }
 }
