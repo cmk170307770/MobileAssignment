c209c96564344697496262c701d22f2af5cebbe8
cmk <cmk170307770@gmail.com>
2022-06-03 18:40:39 +0800

    Add login and register page

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.assignment
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import okhttp3.*
 import org.json.JSONArray
 import org.json.JSONObject
@@ -9,7 +15,8 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
-    private val client = OkHttpClient()
+    private lateinit var fusedLocationClient: FusedLocationProviderClient;
+    private val client = OkHttpClient();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +24,30 @@ class MainActivity : AppCompatActivity() {
 
         var data = callHKOApi("rhrread");
         println( "cmkTest: " + data)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            println("cmk error.");
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                println("cmklkw: " + location.toString());
+            }
     }
 
     fun callHKOApi(dataType: String){
@@ -29,7 +60,7 @@ class MainActivity : AppCompatActivity() {
             override fun onFailure(call: Call, e: IOException) {}
             override fun onResponse(call: Call, response: Response) {
                 var rainfallList = JSONArray(JSONObject(response.body()?.string())["rainfall"]);
-                
+
             }
 
         })
