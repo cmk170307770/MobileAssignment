44620810edd3d6b036eb54c0bd082c94d58a683c
cmk <cmk170307770@gmail.com>
2022-06-11 17:49:51 +0800

    connect to firebase real time database

--- app/src/main/java/com/example/assignment/LogActivity.kt
+++ app/src/main/java/com/example/assignment/LogActivity.kt
@@ -1,8 +1,10 @@
 package com.example.assignment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
@@ -12,19 +14,64 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.assignment.databinding.ActivityLogBinding
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class LogActivity : AppCompatActivity() {
 
     lateinit var _db:DatabaseReference;
+    lateinit var _adapter: TaskAdapter;
+    var _taskList:MutableList<Task>? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log);
 
+        _taskList = mutableListOf();
         _db = FirebaseDatabase.getInstance().reference;
+        _adapter = TaskAdapter(this, _taskList!!);
+        val listviewTask = findViewById<ListView>(R.id.listViewResult);
+        listviewTask!!.setAdapter(_adapter);
+
+        _db.orderByKey().addValueEventListener(_taskListener);
+    }
+
+    var _taskListener: ValueEventListener = object: ValueEventListener{
+        override fun onDataChange(snapshot: DataSnapshot) {
+            loadTaskList(snapshot);
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            Log.d("LogActivity", "loadItem:onCancelled", error.toException());
+        }
+    }
+
+    private fun loadTaskList(dataSnapshot: DataSnapshot){
+        val tasks = dataSnapshot.children.iterator();
+
+        if(tasks.hasNext()){
+            _taskList!!.clear();
+
+            var listIndex = tasks.next();
+            val itemsIterator = listIndex.children.iterator();
+
+            while (itemsIterator.hasNext()){
+                val currentItem = itemsIterator.next();
+                val task = Task.create();
+
+                var map = currentItem.getValue() as HashMap<String, Any>;
+
+                task.objectId = currentItem.key;
+                var createDateTime = LocalDateTime.parse(map.get("createDate") as String);
+                val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                val formatted = createDateTime.format(formatter)
+                task.createDate = formatted;
+                task.number = map.get("number") as String;
+                _taskList!!.add(task);
+            }
+        }
+        _adapter.notifyDataSetChanged();
     }
 
     fun addTask(view: View){
--- app/src/main/java/com/example/assignment/TaskAdapter.kt
+++ app/src/main/java/com/example/assignment/TaskAdapter.kt
@@ -1,4 +1,54 @@
 package com.example.assignment
 
-class TaskAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class TaskAdapter (context: Context, taskList: MutableList<Task>) : BaseAdapter(){
+    private val _inflater: LayoutInflater = LayoutInflater.from(context);
+    private val _tastList = taskList;
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val objectId: String = _tastList.get(position).objectId as String;
+        val createDate: String = _tastList.get(position).createDate as String;
+        val result: String = _tastList.get(position).number as String;
+
+        val view: View;
+        val listRowHolder: ListRowHolder;
+
+        if(convertView == null){
+            view = _inflater.inflate(R.layout.log_row, parent, false);
+            listRowHolder = ListRowHolder(view);
+            view.tag = listRowHolder
+        }else{
+            view = convertView;
+            listRowHolder = view.tag as ListRowHolder;
+        }
+
+        listRowHolder.createDate.text =createDate;
+        listRowHolder.result.text = result;
+
+        return view
+    }
+
+    override fun getCount(): Int {
+        return _tastList.size;
+    }
+
+    override fun getItem(position: Int): Any {
+        return _tastList.get(position);
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong();
+    }
+
+    private class ListRowHolder(row: View?){
+        val createDate: TextView = row!!.findViewById(R.id.createDate);
+        val result: TextView = row!!.findViewById(R.id.resultTestView);
+
+    }
 }
